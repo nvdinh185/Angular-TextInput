4c79960f273d1740a49cbdc41177e11d2f06df85
nvdinh185 <nvdinh0766777123@gmail.com>
2021-04-29 14:39:41 +0700

    edit all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppComponent } from './app.component';
 import { HinhchunhatComponent } from './hinhchunhat/hinhchunhat.component';
 import { DaysoComponent } from './dayso/dayso.component';
 import { HinhtronComponent } from './hinhtron/hinhtron.component';
+import { Hinhtron1Component } from './hinhtron1/hinhtron1.component';
 import { Hinhtron2Component } from './hinhtron2/hinhtron2.component';
 import { ThaythechuoiComponent } from './thaythechuoi/thaythechuoi.component';
 import { Thaythechuoi2Component } from './thaythechuoi2/thaythechuoi2.component';
-import { Hinhtron1Component } from './hinhtron1/hinhtron1.component';
 
 @NgModule({
   declarations: [
--- src/app/hinhchunhat/hinhchunhat.component.ts
+++ src/app/hinhchunhat/hinhchunhat.component.ts
@@ -24,8 +24,10 @@ export class HinhchunhatComponent implements OnInit {
    * @param HCNForm 
    */
   tinhHCN(HCNForm) {
-    if(!Number.isInteger(parseFloat(HCNForm.value.chieuDai))||!Number.isInteger(parseFloat(HCNForm.value.chieuRong))){
-      throw new Error('Phải nhập vào số nguyên!')
+    if (!Number.isInteger(parseFloat(HCNForm.value.chieuDai)) || !Number.isInteger(parseFloat(HCNForm.value.chieuRong))) {
+      // throw new Error('Phải nhập vào số nguyên!');
+      alert('Phải nhập vào số nguyên!');
+      return;
     }
     this.chuVi = 2 * (parseInt(HCNForm.value.chieuDai) + parseInt(HCNForm.value.chieuRong));
     this.dienTich = parseInt(HCNForm.value.chieuDai) * parseInt(HCNForm.value.chieuRong);
